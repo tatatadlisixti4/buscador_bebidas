f8c8be7318172ad8f251e03e46ab70fdd900e3cf
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-27 01:04:59 -0300

    Theming con bg custom para el header en /

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,9 @@ import {NavLink, useLocation} from 'react-router-dom'
 
 export default function Header() {
     const {pathname} = useLocation()
-    console.log(pathname)
     const isHome = useMemo(() => pathname === '/',  [pathname])
-    console.log(isHome)  
     return (
-        <header className="bg-slate-800">
+        <header className={isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
             <div className="mx-auto container px-5 py-16">
                 <div className="flex justify-between items-center">
                     <div>
