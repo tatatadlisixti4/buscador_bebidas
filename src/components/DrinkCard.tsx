7f3eac844af0013301602acb615d744ef01d305c
Thadli Guerra <tg.ibarra64@gmail.com>
2025-02-01 22:57:29 -0300

    Finalizando el código para mostrar las recetas

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -6,8 +6,22 @@ type DrinkCardProps = {
 
 export default function DrinkCard({drink}: DrinkCardProps) {
     return (
-        <div>
-            <h2>{drink.strDrink}</h2>
+        <div className="border shadow-lg">
+            <div className="overflow-hidden">
+                <img 
+                    src={drink.strDrinkThumb} 
+                    alt={`Imagen de ${drink.strDrink} `} 
+                    className="hover:scale-125 transition-transform hover:rotate-2"
+                />
+            </div>
+
+            <div className="p-5">
+                <h2 className="text-2xl truncate">{drink.strDrink}</h2>
+                <button
+                    type="button"
+                    className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+                >Ver Receta</button>
+            </div>
         </div>
     )
 }
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -8,16 +8,16 @@ export default function IndexPage() {
 
     return (
         <>
-            <h1 className="text-6xl font-extrabold">Recetas</h1>
+            <h1 className="text-6xl font-extrabold mb-5">Recetas</h1>
             {hasDrinks ? (
-                <>
+                <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-1' gap-10">
                     {drinks.map(drink => (
                         <DrinkCard
                             key={drink.idDrink}
                             drink={drink}
                         />
                     ))}
-                </>
+                </div>
             ) : (
                 <>
                     <p className="my-10 text-center text-2xl">No hay resultados aún, debes buscar las recetas primero.</p>
